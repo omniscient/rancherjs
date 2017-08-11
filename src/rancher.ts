f054494aeae240af226c6e5ca57a71ee059869e3
Francois Germain <francois@omniscient.ca>
2017-08-10 20:37:53 -0400

    Changed CLI to support commands

--- src/rancher.ts
+++ src/rancher.ts
@@ -3,24 +3,37 @@
 import { RancherJs } from './rancher-js';
 import * as commander from 'commander';
 
-function multiValueHandler(val: string, memo: string[]) {
-  memo.push(val);
-  return memo;
-}
-
 commander
-  .version('0.0.1')
-  .option('-r, --rancher <str>', 'Rancher server URL')
+  .version('0.0.2')
+  .option('-s, --server <str>', 'Rancher server URL. ex: https://my.rancher.com')
   .option('-u, --user <str>', 'Rancher Api User name')
   .option('-p, --password <str>', 'Rancher Api key')
-  .option('-s, --service [value]', 'Rancher Service to be upgraded', multiValueHandler, [])
-  .parse(process.argv);
+  .option('-t, --timeout <n>', 'Overrides the default timeout of 60 seconds on CLI commands', parseInt)
 
-const rancher = new RancherJs(commander['rancher'], commander['user'], commander['password']);
-for (let index = 0; index < commander['service'].length; index++) {
-  rancher.upgrade(commander['service'][index]).then(result => {
-    console.log(`Upgrade success: ${result}`);
-  }, error => {
-    console.log(`Upgrade failed: ${error}`);
-  });
+commander
+   .command('upgrade <service> [otherServices...]')
+   .description('run remote upgrade of one of multiple services by name')
+   .action((service: string, otherServices?: Array<string>) => {
+     let services: Array<string> = [];
+     services.push(service);
+     if (otherServices) services = services.concat(otherServices);
+     
+     console.log(`Starting the upgrade of these services: ${JSON.stringify(services)}`);
+     const rancher = initRancher();
+     console.log(JSON.stringify(rancher));
+     services.forEach(service => {
+       rancher.upgrade(service).then(result => {
+        console.log(`Upgrade success: ${result}`);
+      }, error => {
+        console.log(`Upgrade failed: ${error}`);
+      });
+     });
+   });
+
+commander.parse(process.argv);
+
+function initRancher(): RancherJs {
+  const rancher = new RancherJs(commander['server'], commander['user'], commander['password']);
+  if (commander['timeout']) rancher.timeout = commander['timeout'];
+  return rancher;
 }
